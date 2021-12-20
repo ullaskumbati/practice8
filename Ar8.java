754d9eab98dfcc1287afc0a5eda4ab940b977da1
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-20 21:31:06 +0530

    passed Ar8

---
+++ Ar8.java
@@ -0,0 +1,15 @@
+class Ar8{
+	public static void main(String []as){
+	boolean []hat={true,false,false,true,true,true,false,true,false};
+		System.out.println(hat[1]);
+		System.out.println(hat[7]);
+		System.out.println(hat[5]);
+		System.out.println(hat[8]);
+		System.out.println(hat[3]);
+		System.out.println(hat[2]);
+		System.out.println(hat[4]);
+		System.out.println(hat[6]);
+
+	}
+
+}
